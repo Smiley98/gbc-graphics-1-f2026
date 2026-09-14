f15c9a0cb76bfb51114c10f1d9a4c87588e26703
Smiley98 <smileysofficialcinema@gmail.com>
2026-09-14 15:56:27 -0400

    Added glad and glfw

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,38 @@
-int main()
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+
+int main(void)
 {
-	return 0;
+    GLFWwindow* window;
+
+    /* Initialize the library */
+    if (!glfwInit())
+        return -1;
+
+    /* Create a windowed mode window and its OpenGL context */
+    window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+    if (!window)
+    {
+        glfwTerminate();
+        return -1;
+    }
+
+    /* Make the window's context current */
+    glfwMakeContextCurrent(window);
+
+    /* Loop until the user closes the window */
+    while (!glfwWindowShouldClose(window))
+    {
+        /* Render here */
+        //glClear(GL_COLOR_BUFFER_BIT); // <-- runtime error. We will debug this next time. I will come to class prepared with the solution!
+
+        /* Swap front and back buffers */
+        glfwSwapBuffers(window);
+
+        /* Poll for and process events */
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
+    return 0;
 }
